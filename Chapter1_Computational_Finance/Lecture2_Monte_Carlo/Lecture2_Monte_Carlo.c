99fc9054a3a5eb2136ae613008f4889300325e0f
Mona Lisa <jmhong.2019@gmail.com>
2018-12-16 11:10:32 +0900

    Lecture 2 step 1 begin.

--- Chapter1_Computational_Finance/Lecture2_Monte_Carlo/Lecture2_Monte_Carlo.c
+++ Chapter1_Computational_Finance/Lecture2_Monte_Carlo/Lecture2_Monte_Carlo.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main()
 {
-	printf("Hello, World\n");
+	srand((unsigned int)time(NULL));
+
+	for (int i = 0; i < 100; ++i)
+	{
+		const int random_number = rand();
+
+		printf("%d\n", random_number);
+	}
+
+	// you may try more accurate std::random in c++
 }
 
