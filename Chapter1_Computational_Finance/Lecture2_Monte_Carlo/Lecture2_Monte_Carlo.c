1c092cc01d4e4f3e52fa4f5cd60fc8324a72ba0b
Mona Lisa <jmhong.2019@gmail.com>
2018-12-19 00:30:41 +0900

    Lecture 2 Final

--- Chapter1_Computational_Finance/Lecture2_Monte_Carlo/Lecture2_Monte_Carlo.c
+++ Chapter1_Computational_Finance/Lecture2_Monte_Carlo/Lecture2_Monte_Carlo.c
@@ -1,18 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdbool.h>
 
-int main()
+double get_rand_real(const double min, const double max)
+{
+	double temp = (double)rand() / (double)RAND_MAX; // temp >= 0.0 && temp <= 1.0
+	temp = min + (max - min)*temp; // temp >= min && temp <= max
+
+	return temp;
+}
+
+double est_income()
+{
+	//FILE *of = fopen("annual_salary.txt", "w");
+
+	double annual_salary = 0.3;
+	double income_sum = 0;
+
+	for (int age = 25; age < 55; age++)
+	{
+
+		income_sum += annual_salary;
+
+		//fprintf(of, "%f\n", annual_salary);
+
+		const double salary_increase = get_rand_real(0.0, 0.06);
+
+		annual_salary *= (1.0 + salary_increase);
+	}
+
+	//fclose(of);
+
+	return income_sum;
+}
+
+void main()
 {
 	srand((unsigned int)time(NULL));
 
-	for (int i = 0; i < 100; ++i)
+	int num_samples = 1;
+	for (int i = 0; i < 20; ++i)
 	{
-		const int random_number = rand();
+		double est_inc = 0.0;
 
-		printf("%d\n", random_number);
+		for (int s = 0; s < num_samples; ++s)
+		{
+			est_inc += est_income();
+		}
+
+		printf("%f\n", (double)est_inc / num_samples);
+
+		num_samples *= 2;
 	}
 
-	// you may try more accurate std::random in c++
+	// 3% -> 14.272625
 }
 
